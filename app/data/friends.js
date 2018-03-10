c66cf6809373a2cb0898e22d094a12519f487e45
David Scherer-ODell <dscherer21@gmail.com>
2018-03-10 15:23:42 -0600

    made some changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,41 @@
 var friendsArray = [{
-        name: "The Million Dollar Man Ted Dibiase",
-        photo: "https://s-media-cache-ak0.pinimg.com/originals/a9/09/b4/a909b465a163e1c0b895b6e8843d99bc.jpg",
-        scores: [1, 3, 5, 3, 1, 1, 5, 4, 3, 2]
+        name: "Peter Petrelli",
+        photo: "../../images/peterpetrelli.jpg",
+        scores: [1, 1, 1, 3, 1, 1, 5, 4, 3, 2]
     }, {
-        name: "The Honkey Tonk Man",
-        photo: "http://www.writeups.org/wp-content/uploads/Honky-Tonk-Man-wrestler.jpg",
-        scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+        name: "Claire Bennet",
+        photo: "../../images/clairebennet.jpg",
+        scores: [5, 4, 2, 4, 5, 1, 2, 3, 4, 5]
     },
     {
-        name: "Mr. Perfect",
-        photo: "http://iamjekker.com/wp-content/uploads/2015/06/9156859_orig.jpg",
-        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+        name: "Sylar",
+        photo: "../../images/sylar.jpg",
+        scores: [2, 3, 5, 5, 5, 5, 5, 5, 5, 5]
     },
     {
-        name: "The Macho Man Randy Savage",
-        photo: "https://s-media-cache-ak0.pinimg.com/236x/83/12/7a/83127a08e032b7ecf0ba28ce7c27ba81.jpg",
-        scores: [4, 1, 1, 2, 5, 4, 5, 2, 4, 2]
+        name: "Dr. Mohinder Suresh",
+        photo: "../../images/mohindersuresh.jpg",
+        scores: [3, 1, 3, 2, 5, 4, 5, 2, 4, 2]
     },
     {
-        name: "Jake the Snake Roberts",
-        photo: "http://splitsider.awlnetwork.com/wp-content/uploads/sites/2/2016/09/jake-the-snake-roberts.jpg",
-        scores: [3, 3, 3, 1, 1, 5, 5, 4, 5, 2]
+        name: "Matt Parkman",
+        photo: "../../images/mattparkman.jpg",
+        scores: [1, 3, 1, 1, 1, 5, 5, 4, 5, 2]
     },
     {
-        name: "Andre the Giant",
-        photo: "https://vignette2.wikia.nocookie.net/supergreatfrien/images/4/40/Andre.png/revision/latest?cb=20140709032847",
-        scores: [2, 2, 3, 4, 5, 2, 1, 4, 4, 4]
+        name: "Micah Sanders",
+        photo: "../../images/micahsanders.jpg",
+        scores: [4, 4, 2, 4, 5, 2, 1, 4, 4, 4]
     },
     {
-        name: "Mantaur",
-        photo: "https://ringthedamnbell.files.wordpress.com/2015/11/mantas.jpg",
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        name: "Angela Petrelli",
+        photo: "../../images/angelapetrelli.jpg",
+        scores: [5, 5, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "Hiro Nakamura",
+        photo: "../../images/hironakamura.jpg",
+        scores: [5, 5, 1, 1, 1, 1, 1, 1, 1, 1]
     }
 ];
 
