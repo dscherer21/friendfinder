24b5c19953ffca7b71a4d306262dd10ff4bf454f
David Scherer-ODell <dscherer21@gmail.com>
2018-03-12 15:57:31 -0500

    fixed images issue

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,40 +1,40 @@
 var friendsArray = [{
         name: "Peter Petrelli",
-        photo: "http://coolspotters.com/characters/peter-petrelli",
+        photo: "images/peterpetrelli.jpg",
         scores: [1, 1, 1, 1, 1, 1, 1, 2, 1, 2]
     }, {
         name: "Claire Bennet",
-        photo: "http://heroes.wikia.com/wiki/Claire_Bennet",
+        photo: "images/clairebennet.jpg",
         scores: [4, 4, 2, 4, 5, 1, 3, 2, 2, 1]
     },
     {
         name: "Sylar",
-        photo: "http://heroes.wikia.com/wiki/Gabriel_Gray",
+        photo: "images/sylar.jpg",
         scores: [1, 1, 4, 5, 4, 5, 4, 1, 4, 5]
     },
     {
         name: "Dr. Mohinder Suresh",
-        photo: "https://nerdist.com/tv-cap-heroes-reborn-brings-back-mohinder-suresh-fox-announces-comic-con-tv-lineup-more/",
+        photo: "images/mohindersuresh.jpg",
         scores: [3, 1, 3, 4, 3, 2, 2, 4, 1, 4]
     },
     {
         name: "Matt Parkman",
-        photo: "http://powerlisting.wikia.com/wiki/File:Matt-Parkman.jpg",
+        photo: "images/mattparkman.jpg",
         scores: [1, 3, 2, 2, 5, 4, 4, 3, 1, 3]
     },
     {
         name: "Micah Sanders",
-        photo: "https://www.youtube.com/watch?v=HmvG2icaX9o",
+        photo: "images/micahsanders.jpg",
         scores: [5, 5, 2, 2, 3, 4, 1, 3, 3, 3]
     },
     {
         name: "Angela Petrelli",
-        photo: "https://www.youtube.com/watch?v=sgbz-d_EGWQ",
+        photo: "images/angelapetrelli.jpg",
         scores: [5, 5, 2, 4, 1, 3, 5, 5, 5, 1]
     },
     {
         name: "Hiro Nakamura",
-        photo: "https://www.polygon.com/2015/3/27/8302645/masi-oka-hiro-heroes-reborn",
+        photo: "images/hironakamura.jpg",
         scores: [5, 4, 3, 1, 5, 4, 5, 3, 1, 3]
     }
 ];
