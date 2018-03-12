d354ad61622e0d791940d9f5f1235f0eadd516da
David Scherer-ODell <dscherer21@gmail.com>
2018-03-12 01:02:18 -0500

    chagned some things

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,41 +1,41 @@
 var friendsArray = [{
         name: "Peter Petrelli",
-        photo: "../../images/peterpetrelli.jpg",
-        scores: [1, 1, 1, 3, 1, 1, 5, 4, 3, 2]
+        photo: "http://coolspotters.com/characters/peter-petrelli",
+        scores: [1, 1, 1, 1, 1, 1, 1, 2, 1, 2]
     }, {
         name: "Claire Bennet",
-        photo: "../../images/clairebennet.jpg",
-        scores: [5, 4, 2, 4, 5, 1, 2, 3, 4, 5]
+        photo: "http://heroes.wikia.com/wiki/Claire_Bennet",
+        scores: [4, 4, 2, 4, 5, 1, 3, 2, 2, 1]
     },
     {
         name: "Sylar",
-        photo: "../../images/sylar.jpg",
-        scores: [2, 3, 5, 5, 5, 5, 5, 5, 5, 5]
+        photo: "http://heroes.wikia.com/wiki/Gabriel_Gray",
+        scores: [1, 1, 4, 5, 4, 5, 4, 1, 4, 5]
     },
     {
         name: "Dr. Mohinder Suresh",
-        photo: "../../images/mohindersuresh.jpg",
-        scores: [3, 1, 3, 2, 5, 4, 5, 2, 4, 2]
+        photo: "https://nerdist.com/tv-cap-heroes-reborn-brings-back-mohinder-suresh-fox-announces-comic-con-tv-lineup-more/",
+        scores: [3, 1, 3, 4, 3, 2, 2, 4, 1, 4]
     },
     {
         name: "Matt Parkman",
-        photo: "../../images/mattparkman.jpg",
-        scores: [1, 3, 1, 1, 1, 5, 5, 4, 5, 2]
+        photo: "http://powerlisting.wikia.com/wiki/File:Matt-Parkman.jpg",
+        scores: [1, 3, 2, 2, 5, 4, 4, 3, 1, 3]
     },
     {
         name: "Micah Sanders",
-        photo: "../../images/micahsanders.jpg",
-        scores: [4, 4, 2, 4, 5, 2, 1, 4, 4, 4]
+        photo: "https://www.youtube.com/watch?v=HmvG2icaX9o",
+        scores: [5, 5, 2, 2, 3, 4, 1, 3, 3, 3]
     },
     {
         name: "Angela Petrelli",
-        photo: "../../images/angelapetrelli.jpg",
-        scores: [5, 5, 1, 1, 1, 1, 1, 1, 1, 1]
+        photo: "https://www.youtube.com/watch?v=sgbz-d_EGWQ",
+        scores: [5, 5, 2, 4, 1, 3, 5, 5, 5, 1]
     },
     {
         name: "Hiro Nakamura",
-        photo: "../../images/hironakamura.jpg",
-        scores: [5, 5, 1, 1, 1, 1, 1, 1, 1, 1]
+        photo: "https://www.polygon.com/2015/3/27/8302645/masi-oka-hiro-heroes-reborn",
+        scores: [5, 4, 3, 1, 5, 4, 5, 3, 1, 3]
     }
 ];
 
